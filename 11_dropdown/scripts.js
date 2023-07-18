5b1106755b6f8bfb546b3119ba6589ab7abfed75
wxsleyb <wesleyfbraga@gmail.com>
2023-07-18 17:01:20 -0300

    html css js dropdown

---
+++ 11_dropdown/scripts.js
@@ -0,0 +1,23 @@
+const backToTopBtn = document.querySelector("#back-to-top")
+
+window.addEventListener("scroll", function(){
+    const scrollTop = document.documentElement.scrollTop;
+
+    if(scrollTop>500){
+        backToTopBtn.style.display = "block";
+    } else {
+        backToTopBtn.style.display = "none"
+    }
+
+backToTopBtn.addEventListener("click", function(e){
+    e.preventDefault()
+
+    window.scrollTo({
+        top:0,
+        behavior: "smooth",
+    })
+
+})
+
+})
+
